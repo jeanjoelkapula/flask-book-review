05b25b7d39abf73e5ec04ed7b73ddbdb86a974c5
jeanjoel <jeanjoelkapula@gmail.com>
2019-11-29 14:39:57 +0200

    Complete initial page designs

---
+++ env/Application/static/scripts/interface.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("mainNav");
+var errormessages = $('.error-message');
+
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+$( document ).ready(function() {
+  $('.error-message').each(function(i, obj) {
+      obj.style.display = 'none';
+  });
+
+});
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    //alert('hey');
+  } else {
+    //navbar.classList.remove("sticky");
+  }
+}
+
+function menubuttonclick () {
+  //$('#mainNav').toggle('slow');
+  header = document.getElementById("loginsignin-header");
+  console.log('header '+ window.getComputedStyle(header).height + 'navbar' + window.getComputedStyle(navbar).height);
+  //alert('header ' + header.height + 'Nav:' + navbar.height);
+}
